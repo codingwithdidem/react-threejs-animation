9883b2d7575820a61f32b8240a503c16bdaffc24
didem <didem.kucukkaraaslan@altosec.com.tr>
2021-09-03 17:03:15 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
+import { OrbitControls, Stars } from '@react-three/drei'
+import Cat from './components/Cat'
 
-function App() {
+function Box(props) {
+  const texture = useLoader(
+    TextureLoader,
+    'https://letsenhance.io/static/334225cab5be263aad8e3894809594ce/75c5a/MainAfter.jpg'
+  )
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+    ref.current.rotation.z += 0.01
+  })
+  // Return the view, these are regular Threejs elements expressed in JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh
+      {...props}
+      ref={ref}
+      // scale={active ? 1.5 : 1}
+      position={active ? [0, 1, 0] : [0, 0, 0]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <dodecahedronBufferGeometry args={[1, 1, 1]} />
+      <meshNormalMaterial
+        color={hovered ? 'hotpink' : '#c814ee'}
+        normalMap={texture}
+      />
+    </mesh>
+  )
 }
 
-export default App;
+const App = () => {
+  return (
+    <>
+      <Canvas>
+        <Stars />
+        <OrbitControls />
+        <ambientLight />
+
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback='Loading..'>
+          {/* <Box position={[0, 0, 0]} /> */}
+          <Cat />
+        </Suspense>
+      </Canvas>
+
+      <h1 style={{ color: '#fff' }}>Hello, I am Didem.</h1>
+    </>
+  )
+}
+
+export default App
